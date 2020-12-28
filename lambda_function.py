c3a2c68519931a652b8064bceefcb7aba7b2ee30
Fayçal <faycal.zouaoui@finalcad.com>
2020-12-28 12:55:10 +0100

    hello world test4

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,15 @@
+from flask_api import FlaskAPI, status
+
 def lambda_handler(event, context):
     message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
     return { 
         'message' : message
     }
+
+app = FlaskAPI(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return "", status.HTTP_200_OK
+app.run()
